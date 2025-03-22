f0676f4e347c491188c436ed20b7c2149e9284e3
Aaradhya Agrawal <aaradhya@Aaradhyas-MacBook-Air.local>
2025-03-22 09:19:45 +0530

    Updated Portfolio

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Example: Smooth scroll for anchor links
+// Smooth scroll for anchor links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
